0f8dc6a8b41bf919285491f8c5f68047a0131a80
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-27 15:12:05 +0800

    Possible GWA

--- Students-GWA.py
+++ Students-GWA.py
@@ -1,2 +1,9 @@
 # ELLA MARIE A. MALLARI
 # BSCPE 1-4
+
+# STUDENTS GWA
+with open("students_gwa.txt") as input_file:
+    # possible lowest students gwa
+    highest_gwa = 4.00
+    highest_student = ""
+    
