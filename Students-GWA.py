5d09c934f9dd2f742c4f93c1a3e5805d0f12b33d
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-27 15:15:25 +0800

    Possible GWA

--- Students-GWA.py
+++ Students-GWA.py
@@ -6,4 +6,6 @@ with open("students_gwa.txt") as input_file:
     # possible lowest students gwa
     highest_gwa = 4.00
     highest_student = ""
-    
+    # read the txt.file (students_gwa.txt) and split it 
+    for line in input_file:
+        
